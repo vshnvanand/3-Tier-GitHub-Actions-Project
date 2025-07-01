129349ce3c99a871170ee805bf8bd7cf74c35da3
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2025-07-01 13:11:46 +0530

    Update db.js

--- api/models/db.js
+++ api/models/db.js
@@ -1,16 +1,18 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-  if (err) throw err;
-  console.log('MySQL Connected');
-});
+const db = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
 module.exports = db;
